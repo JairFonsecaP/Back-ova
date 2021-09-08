280ef9702ca2c054b5693cd7de9b039d71d1261d
JairFonsecaP <jairfonseca64@yahoo.esa>
2021-09-08 12:54:33 -0500

    Coneccion correcta a la base de datos

--- controllers/contactoController.js
+++ controllers/contactoController.js
@@ -2,18 +2,23 @@ const db = require("../database/models");
 const email = require("../services/email");
 
 exports.registro = async (req, res) => {
-  const response = await db.Contact.create(req.body);
-  const emails = await email.contact(response.toJSON());
-  emails
-    ? res.send({
-        type: "success",
-        title: "Registro exitoso",
-        message:
-          "Gracias por comunicarte con nosotros en breve alguien de nuestro equipo se contactará contigo",
-      })
-    : res.send({
-        type: "error",
-        title: "Error",
-        message: "No se pudo enviar su mensaje, por favor intente nuevamente.",
-      });
+  try {
+    const response = await db.Contact.create(req.body);
+    const emails = await email.contact(response.toJSON());
+    emails
+      ? res.send({
+          type: "success",
+          title: "Registro exitoso",
+          message:
+            "Gracias por comunicarte con nosotros en breve alguien de nuestro equipo se contactará contigo",
+        })
+      : res.send({
+          type: "error",
+          title: "Error",
+          message:
+            "No se pudo enviar su mensaje, por favor intente nuevamente.",
+        });
+  } catch (e) {
+    console.log(e);
+  }
 };
--- database/config/config.js
+++ database/config/config.js
@@ -1,25 +1,32 @@
 module.exports = {
   development: {
-    username: "wtlGBn7iaf",
-    password: "sd1gLl5UKt",
-    database: "wtlGBn7iaf",
-    host: "remotemysql.com",
-    dialect: "mysql",
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
-  production: {
     host: "ec2-44-198-80-194.compute-1.amazonaws.com",
-    port: "5432",
+    /*port: 5432,*/
     database: "dfi0aveu2dh9m0",
     username: "twrywdbyqfifyd",
     password:
       "3d23ecea7a718b70263366cfa8894d02cdfc5d835707cd4e3944d5cc5fe73f3b",
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+  production: {
+    host: "ec2-44-198-80-194.compute-1.amazonaws.com",
+    /*port: 5432,*/
+    database: "dfi0aveu2dh9m0",
+    username: "twrywdbyqfifyd",
+    password:
+      "3d23ecea7a718b70263366cfa8894d02cdfc5d835707cd4e3944d5cc5fe73f3b",
+    dialect: "postgres",
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
 };
