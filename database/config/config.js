44a0af31227f2aed04d05c7f31eea107242ec152
JairFonsecaP <jairfonseca64@yahoo.esa>
2021-09-18 12:06:53 -0500

    Corrección de configuracion de coneccion db

--- database/config/config.js
+++ database/config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  development: {
-    host: "localhost",
-    /*port: 5432,*/
-    database: "ova",
-    username: "root",
-    password: null,
-    dialect: "mysql",
-  },
+  // development: {
+  //   host: "localhost",
+  //   /*port: 5432,*/
+  //   database: "ova",
+  //   username: "root",
+  //   password: null,
+  //   dialect: "mysql",
+  // },
   production: {
     host: "ec2-44-198-80-194.compute-1.amazonaws.com",
     /*port: 5432,*/
@@ -15,11 +15,28 @@ module.exports = {
     password:
       "3d23ecea7a718b70263366cfa8894d02cdfc5d835707cd4e3944d5cc5fe73f3b",
     dialect: "postgres",
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false,
-  //   },
-  // },
+  development: {
+    host: "ec2-44-198-80-194.compute-1.amazonaws.com",
+    /*port: 5432,*/
+    database: "dfi0aveu2dh9m0",
+    username: "twrywdbyqfifyd",
+    password:
+      "3d23ecea7a718b70263366cfa8894d02cdfc5d835707cd4e3944d5cc5fe73f3b",
+    dialect: "postgres",
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
